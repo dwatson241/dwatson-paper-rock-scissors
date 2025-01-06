ea1604417a2e5ebcd5222ad4ea5669bbda66fb03
Dwatson241 <derek@derekwatson.design>
2025-01-06 13:50:03 -0700

    Write functions in app.js for game play

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 
-console.log('hello world')
+// console.log('hello world');
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
+}
+console.log(getComputerChoice());
+
+
+function getHumanChoice() {
+  const choices = ["paper", "rock", "scissors"];
+  prompt("paper, rock, or scissors?");
+}
+console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = prompt().toLowerCase
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
