459af72b4d11318b1b96418814d4d07dbb2f6250
Dwatson241 <derek@derekwatson.design>
2025-01-17 11:55:11 -0700

    Finalized app.js code to play game per assignment

--- app.js
+++ app.js
@@ -1,27 +1,82 @@
-
-// console.log('hello world');
-
-let humanScore = 0;
-let computerScore = 0;
-
+// GET COMPUTER CHOICE
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  return choices[Math.floor(Math.random() * 3)];
+  const choice = Math.floor(Math.random() * 3) + 1;
+  if (choice === 1) {
+    return "paper";
+  } else if (choice === 2) {
+    return "rock";
+  } else choice === 3;
+  return "scissors";
 }
-console.log(getComputerChoice());
 
-
+// GET HUMAN CHOICE
 function getHumanChoice() {
-  const choices = ["paper", "rock", "scissors"];
-  prompt("paper, rock, or scissors?");
+  let choice = prompt("Paper, Rock, or Scissors").toLowerCase(); // getHumanChoice INPUT CONVERTED TO LOWER CASE
+  if (choice === "rock" || choice === "paper" || choice === "scissors") {
+    return choice;
+  } else {
+    return null; //
+  }
 }
-console.log(getHumanChoice())
 
+// PLAY SINGLE ROUND
 function playRound(humanChoice, computerChoice) {
-  humanChoice = prompt().toLowerCase
+  if (humanChoice === computerChoice) {
+    return null; // null = tie
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    return 1; // victory
+  } else {
+    return -1; // loss
+  }
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+// PLAY 5 ROUNDS
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
+  for (let round = 1; round <= 5; round++) {
+    console.log(`Round ${round}`);
+    const humanChoice = getHumanChoice();
+
+    if (!humanChoice) {
+      console.log("Please enter paper, rock, or scissors");
+
+      round--; // STOPS ROUND COUNTER FROM ADVANCING IF INPUT IS INVALID
+      continue;
+    }
+
+    const computerChoice = getComputerChoice();
+    console.log(`You chose: ${humanChoice}`); //DISPLAY humanCHOICE
+
+    console.log(`The computer chose: ${computerChoice}`); // DISPLAYS computerChoice
+
+    const result = playRound(humanChoice, computerChoice);
+
+    if (result === null) {
+      console.log("Draw! No points awarded.");
+    } else if (result === 1) {
+      console.log(`You won round! ${round}!`);
+      humanScore++;
+    } else if (result === -1) {
+      console.log(`Computer won round ${round}!`);
+      computerScore++;
+    }
+  }
+
+  // PRINT GAME WINNER TO CONSOLE
+  if (humanScore > computerScore) {
+    console.log("You won! Play again? Refresh the page!");
+  } else if (humanScore < computerScore) {
+    console.log("You lose... refresh the page to play again!");
+  } else {
+    console.log("Draw! No winner. Play again? Refresh the page!");
+  }
+}
+
+// PLAY GAME WITH BELOW
+playGame();
